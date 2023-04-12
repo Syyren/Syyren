bccefc12767847b33d6c970caf1d276ca4156c16
Syyren <129496054+Syyren@users.noreply.github.com>
2023-04-12 13:43:05 -0600

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,5 @@
 window.onscroll = function() {
   myFunction();
-  myFunctionForPageNav();
 };
 
 var topnav = document.getElementById("mynav");
@@ -11,16 +10,5 @@ function myFunction() {
       topnav.classList.add("sticky");
   } else {
       topnav.classList.remove("sticky");
-  }
-}
-
-var card = document.getElementById("pagenav");
-var stickyPageNav = card.offsetTop;
-
-function myFunctionForPageNav() {
-  if (window.pageYOffset > stickyPageNav) {
-      card.classList.add("sticky-pagenav");
-  } else {
-      card.classList.remove("sticky-pagenav");
   }
 }
